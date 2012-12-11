7e5c19f67f3b4e909705b9359156999e732e39f7
dpramita21@gmail.com <dpramita21@gmail.com@5e4d745d-4fec-68d0-437e-23056d3ac864>
2012-12-11 16:19:18 +0000

    + code u/ mengambil nama file, size fle, catch error, dan memindahkan image ke suatu folder

--- upload_poster.php
+++ upload_poster.php
@@ -7,10 +7,17 @@ mysql_select_db ("hos");
 	@$tanggal=$_POST ['tanggal'];
 	@$tempat=$_POST ['tempat'];
 	@$catatan=$_POST ['catatan'];
-	//@$poster=$_POST ['picfotoe'];
+	@$PosterName=null;
+   
+   @$PosterName = $_FILES['picfotoe']['name']; //get the file name
+	  @$PosterSize = $_FILES['picfotoe']['size']; //get the size
+	  @$PosterError = $_FILES['picfotoe']['error']; //get the error when upload
+			if($PosterSize > 0 || $PosterError == 0){ //check if the file is corrupt or error
+				$move = move_uploaded_file($_FILES['picfotoe']['tmp_name'], 'poster/'.$PosterName); //save image to the folder
+				}
 	
-		if (mysql_query("INSERT INTO event (nama_event, deskripsi_event, tanggal_event, tempat_event, catatan_tambahan_event)
-			VALUES('$namae', '$deskripsi', '$tanggal', '$tempat', '$catatan')"))
+		if (mysql_query("INSERT INTO event (nama_event, deskripsi_event, tanggal_event, tempat_event, catatan_tambahan_event, poster_event)
+			VALUES('$namae', '$deskripsi', '$tanggal', '$tempat', '$catatan', '$PosterName')"))
 			{
 			header("location: home.php");
 			}else{
