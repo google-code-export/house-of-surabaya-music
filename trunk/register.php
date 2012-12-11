d0b96fbccd7f115fd6a9dbc0e2be263798443af1
dpramita21@gmail.com <dpramita21@gmail.com@5e4d745d-4fec-68d0-437e-23056d3ac864>
2012-12-11 16:23:18 +0000

    + upload foto fasilitas

--- trunk/register.php
+++ trunk/register.php
@@ -7,7 +7,8 @@ mysql_select_db ("hos");
 	@$noktp=$_POST ['no_KTP'];
 	@$password=$_POST ['password'];
 	@$tipef=$_POST ['tipef'];
-	@$Sipuname = null;
+	@$SipuName=null;
+	@$FotoName=null;
 			
 		@$SipuName = $_FILES['picsipu']['name']; //get the file name
 		@$SipuSize = $_FILES['picsipu']['size']; //get the size
@@ -15,16 +16,23 @@ mysql_select_db ("hos");
 			if($SipuSize > 0 || $SipuError == 0){ //check if the file is corrupt or error
 				$move = move_uploaded_file($_FILES['picsipu']['tmp_name'], 'image/'.$SipuName); //save image to the folder
 				}
+				
+		@$FotoName = $_FILES['picfoto']['name']; //get the file name
+		@$FotoSize = $_FILES['picfoto']['size']; //get the size
+		@$FotoError = $_FILES['picfoto']['error']; //get the error when upload
+			if($FotoSize > 0 || $FotoError == 0){ //check if the file is corrupt or error
+				$move = move_uploaded_file($_FILES['picfoto']['tmp_name'], 'picture/'.$SipuName); //save image to the folder
+				}
 								
 
-		if (mysql_query("INSERT INTO member (nama_fasilitas, nama_pemilik, no_KTP, Password, Tipe_Fasilitas, namasipu)
-			VALUES('$namaf', '$namap', '$noktp', '$password', '$tipef','$SipuName')"))
+		if (mysql_query("INSERT INTO member (nama_fasilitas, nama_pemilik, no_KTP, Password, Tipe_Fasilitas, nama_sipu, foto_fasilitas)
+			VALUES('$namaf', '$namap', '$noktp', '$password', '$tipef', '$SipuName', '$FotoName')"))
 			{
-				if ($tipef=='Studio Musik') {
+				if ($tipef='Studio Musik') {
 					header("location: formstudiomusik.html");}
-				if($tipef=='Studio Recording'){
-					header("location: formstudiorecorder.html");}
-				if($tipef=='Toko Alat Musik') {
+				if($tipef='Studio Recording'){
+					header("location: formstudiorecording.html");}
+				if($tipef='Toko Alat Musik') {
 					header ("location: formtokoalatmusik.html");}
 			
 			}else{
