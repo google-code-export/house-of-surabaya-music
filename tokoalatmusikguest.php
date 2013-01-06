f71854eb33709c74b389e49f896be59b6704ffa4
nhieea@gmail.com <nhieea@gmail.com@5e4d745d-4fec-68d0-437e-23056d3ac864>
2013-01-06 14:40:35 +0000

    menampilkan list toko alat musik dan infonya yang bisa dilihat oleh guest

---
+++ tokoalatmusikguest.php
@@ -0,0 +1,126 @@
+<html>
+<head>
+	<link type"text/css" rel="stylesheet" href="css/bootstrap.css"/>
+<title>member</title>
+<style>
+body
+{
+background-image:url('musik2.jpg');
+background-repeat:no-repeat;
+background-attachment:fixed;
+background-position:bottom right;
+} 
+
+#list
+{
+font-family:"Cambria";
+width:70%;
+border-collapse:collapse;
+}
+#list td, #list th 
+{
+font-size:1em;
+border:2px solid black;
+padding:3px 7px 2px 7px;
+}
+#list th 
+{
+font-size:1.1em;
+text-align:center;
+padding-top:5px;
+padding-bottom:4px;
+background-color:#B0E0E6;
+color:#black;
+}
+#list tr.alt td 
+{
+color:white;
+background-color:#EAF2D3;
+}
+
+</style>
+</head>
+<body>
+
+    <img align="right" src="hos.jpg" width="270" height="300">
+	<div class="hero-unit">
+	<h1 style="color:khaki"><font face="impact">H O S</font></h1> 
+    <p style="color:LemonChiffon "><font face="matura mt script capitals" size="6"><strong>House Of Surabaya Music</strong></font></p>
+	</div>
+	
+    <div class="navbar">
+	<div class="navbar-inner">
+	<a class="brand" href="login.php">HOS</a>
+	<ul class="nav">
+	<li><a href="studiomusikguest.php">Studio Musik</a></li>
+	<li><a href="studiorecordingguest.php">Studio Recording</a></li>
+	<li class="active"><a href="tokoalatmusikguest.php">Toko Alat Musik</a></li>
+	<li><a href="eventguest.php">Upload Poster Event</a></li>
+    </ul>
+	</div>
+	</div>
+
+<center><hr><table border="1" width="100%" cellspacing="1" cellpadding="0" id="list">
+<center><h3><font face="cambria">DAFTAR TOKO ALAT MUSIK SURABAYA</font></h3></center>
+<tr class="alt">
+<th align="center"><b>Nama</b></th>
+<th align="center"><b>Alamat</b></th>
+<th align="center"><b>Wilayah</b></th>
+<th align="center"><b>Telepon</b></th>
+<th align="center"><b>Available Time</b></th>
+<th align="center"><b>Sewa Per Shift</b></th>
+</tr>
+<?
+mysql_connect("localhost","root","");
+mysql_select_db("hos");
+
+@$namasm = $_POST['nama_sm'];
+@$alamatsm = $_POST['alamat_sm'];
+@$wilayahsm = $_POST['wilayah_sm'];
+@$tlpnsm = $_POST['tlpn_sm'];
+@$time = $_POST['available_time_sm'];
+@$sewa = $_POST['sewa_pershift_sm'];
+@$catatan = $_POST['catatan_tambahan'];
+
+$cari="SELECT * FROM toko_alat_musik";
+$hasil=mysql_query($cari);
+while($data=mysql_fetch_row($hasil)){
+		echo"<tr>
+
+		<td>$data[1]</td>
+		<td>$data[2]</td>
+		<td>$data[3]</td>
+		<td>$data[4]</td>
+		<td>$data[5]</td>
+		<td>$data[6]</td>
+			
+		</tr>";
+}
+
+?>
+
+</table>
+
+<!-- Footer-->
+	<footer class="footer">
+	  <br>
+	  <br>
+	  <br>
+	  <br>
+	  <br>
+	  <br>
+	  <br>
+	  <br>
+ 	  <br>
+	  <br>
+	  <br>
+   	  <br>
+	  <br>
+	  <br>
+      <div class="span12">
+        <center>© 2012 House Of Surabaya Music. All rights reserved.</center>
+      </div>
+    </footer>
+</br><script src="js/bootstrap.js"></script>
+</body>
+</html>
