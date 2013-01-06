f5c77bfbbf4e92697dc2fff55e4eb7ea8c3e5ecf
dpramita21@gmail.com <dpramita21@gmail.com@5e4d745d-4fec-68d0-437e-23056d3ac864>
2013-01-06 13:50:07 +0000

    + link delete untuk tiap baris event

--- home.php
+++ home.php
@@ -1,18 +1,21 @@
-
+<?php
+session_start();
+if  (!isset($_SESSION['username'])){
+	header('location:loginforadmin.php');
+	exit;
+	}
+?>
 
 <html>
 <title>HOS</title>
 <head>
 	<link type"text/css" rel="stylesheet" href="css/bootstrap.css"/>
-	
 </head>
 <body>
-
-  <div class="hero-unit">
-    <h1 style="color:white">HOS<img align="right" src="linux.jpg" alt="Smiley face" width="200" height="200"></h1> 
-    <p style="color:yellow">
-	House Of Surabaya Music
-	</p>
+    <img align="right" src="hos.jpg" width="270" height="300">
+	<div class="hero-unit">
+	<h1 style="color:khaki"><font face="impact">H O S</font></h1> 
+    <p style="color:LemonChiffon "><font face="matura mt script capitals" size="6"><strong>House Of Surabaya Music</strong></font></p>
 	</div>
 	
     <div class="navbar">
@@ -23,17 +26,15 @@
     <li><a href="studiomusik.php">Studio Musik</a></li>
 	<li><a href="studiorecording.php">Studio Recording</a></li>
 	<li><a href="tokoalatmusik.php">Toko Alat Musik</a></li>
-    <li><a href="event.html">Upload Poster Event</a></li>
-	<li><a href="logout.php">Logout</a></li>
+    <li><a href="event.php">Upload Poster Event</a></li>
+	<li><a href="logout_admin.php">Logout</a></li>
     </ul>
 	</div>
 	</div>
-	
-
 <style>
 body
 		{
-		background-image:url('music.jpg');
+		background-image:url('musik2.jpg');
 		background-repeat:no-repeat;
 		background-attachment:fixed;
 		background-position:bottom right;
@@ -57,8 +58,8 @@ font-size:1.1em;
 text-align:center;
 padding-top:5px;
 padding-bottom:4px;
-background-color:#A7C942;
-color:#ffffff;
+background-color:#B0E0E6;
+color:black;
 }
 #list tr.alt td 
 {
@@ -69,15 +70,16 @@ background-color:#EAF2D3;
 </style>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
 
+<tr class="alt">
 <center><hr><table border="1" width="100%" cellspacing="1" cellpadding="0" id="list">
 <center><h2>DAFTAR EVENT MUSIK SURABAYA</h2></center>
-<tr class="alt">
 <th align="center"><b>Nama</b></th>
 <th align="center"><b>Deskripsi</b></th>
 <th align="center"><b>Tanggal</b></th>
 <th align="center"><b>Tempat</b></th>
 <th align="center"><b>Poster</b></th>
 <th align="center"><b>Catatan Tambahan</b></th>
+<th align="center"><b>Action</b></th>
 </tr>
 
 <?php
@@ -86,11 +88,54 @@ mysql_select_db("hos");
 $cari="SELECT * FROM event";
 $hasil=mysql_query($cari);
 while($data=mysql_fetch_row($hasil)){
-		echo"<tr><td>".$data[1]."</td><td>".$data[2]."</td><td>".$data[3]."</td><td>".$data[4]."</td><td>".$data[5]."</td><td>".$data[6]."</td></tr>";
-}
+		echo"<tr>
+
+		<td><center>$data[1]</center></td>
+		<td><center>$data[2]</center></td>
+		<td><center>$data[3]</center></td>
+		<td><center>$data[4]</center></td>
+		<td align=center><img src='$data[5]'></td>
+		<td><center>$data[6]</center></td>
+		<td><center><img src='del.jpg' width='40' height='40' onclick='del($data[0])' title='delete'></center></td>	
+		</td>	
+		</tr>";
+
+		}
 
 ?>
-</table>	
+</table>
+</br>
+
+<!-- Footer-->
+	<footer class="footer">
+	  <br>
+	  <br>
+	  <br>
+	  <br>
+	  <br>
+	  <br>
+	  <br>
+	  <br>
+ 	  <br>
+	  <br>
+	  <br>
+   	  <br>
+	  <br>
+	  <br>
+      <div class="span12">
+        <center>© 2012 House Of Surabaya Music. All rights reserved.</center>
+      </div>
+    </footer>
+
+
 <script src="js/bootstrap.js"></script>
+<script type="text/JavaScript">
+function del(id){
+var agree=confirm("Are you sure you want to delete this file?");
+if(agree==true){
+       window.location = 'delete_event.php?id=' + id;
+}
+}
+</script>
 </body>
 </html>
