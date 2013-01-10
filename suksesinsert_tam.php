86fcc3f10050857a43bf04a2c562f790535e478b
nhieea@gmail.com <nhieea@gmail.com@5e4d745d-4fec-68d0-437e-23056d3ac864>
2013-01-10 23:05:08 +0000

    code yang digunakan ketika member toko alat musik berhasil melakukan insert pada form edit di profil toko alat musik.

---
+++ suksesinsert_tam.php
@@ -0,0 +1,71 @@
+<html>
+<title>HOS</title>
+<head>
+	<link type"text/css" rel="stylesheet" href="css/bootstrap.css"/>
+	
+</head>
+<body>
+
+<style>
+body
+		{
+		background-image:url('musik2.jpg');
+		background-repeat:no-repeat;
+		background-attachment:fixed;
+		background-position:bottom right;
+		}
+</style>
+
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+
+<br>
+
+    <img align="right" src="hos.jpg" width="270" height="300">
+	<div class="hero-unit">
+	<h1 style="color:khaki"><font face="impact">H O S</font></h1> 
+    <p style="color:LemonChiffon "><font face="matura mt script capitals" size="6"><strong>House Of Surabaya Music</strong></font></p>
+	</div>
+
+	
+    <div class="navbar navbar-inverse">
+	<div class="navbar-inner">
+	<a class="brand">HOS</a>
+	<ul class="nav">
+	<li class="active"><a href="suksesinsert_tam.php">Toko Alat Musik</a></li>
+	<li><a href="logout.php">Logout</a></li>
+    </ul>
+	</div>
+	</div>
+
+	<center><button type="btn" class="btn btn-success">
+	Register Succed!</button></center>
+	</br>
+		<h5 align="center">Silahkan logout dan kembali login untuk dapat mengkases profil Anda.<h5>
+
+	
+
+<!-- Footer-->
+	<footer class="footer">
+	  <br>
+	  <br>
+	  <br>
+	  <br>
+	  <br>
+	  <br>
+	  <br>
+	  <br>
+ 	  <br>
+	  <br>
+	  <br>
+   	  <br>
+	  <br>
+	  <br>
+      <div class="span12">
+        <center>© 2012 House Of Surabaya Music. All rights reserved.</center>
+      </div>
+    </footer>
+		
+<script src="js/bootstrap.js"></script>
+
+</body>
+</html>
